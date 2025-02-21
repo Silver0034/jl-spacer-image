04eb625986134a03a48c53c8c901cb38575c4471
Jacob Lodes <jlodes@earlenterprise.com>
2025-02-21 14:25:31 -0500

    Remove hello world console log

--- src/jl-spacer-image/view.js
+++ src/jl-spacer-image/view.js
@@ -21,5 +21,5 @@
  */
 
 /* eslint-disable no-console */
-console.log( 'Hello World! (from create-block-jl-spacer-image block)' );
+// console.log( 'Hello World! (from create-block-jl-spacer-image block)' );
 /* eslint-enable no-console */
